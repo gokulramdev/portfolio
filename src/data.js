ce3f701019be9652705f50bb3e308cd7809da8d4
Gokulram <gokulramcse@gmail.com>
2021-12-12 20:44:59 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    link: "",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1639244734551-c8ff9ffe500e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    link: "",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
