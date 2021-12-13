74523145cda06cd491a99f8152714dff2580609a
Gokulram <gokulramcse@gmail.com>
2021-12-13 12:46:50 +0530

    Data changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -7,11 +7,11 @@ const Intro = () => {
       <div className="i-left">
         <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is</h2>
-          <h1 className="i-name">John Burton</h1>
+          <h1 className="i-name">Sowmiya </h1>
           <div className="i-title">
             <div className="i-title-wrapper">
               <div className="i-title-item">Web Developer</div>
-              <div className="i-title-item">UI/UX Designer</div>
+              <div className="i-title-item"> Designer</div>
               <div className="i-title-item">Photographer</div>
               <div className="i-title-item">Writer</div>
               <div className="i-title-item">Content Creator</div>
--- src/data.js
+++ src/data.js
@@ -11,22 +11,22 @@ export const products = [
   },
   {
     id: 3,
-    img: "",
-    link: "http://lama.dev",
+    img: "https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3lzdGVtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    link: "",
   },
   {
     id: 4,
-    img: "",
-    link: "http://lama.dev",
+    img: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c3lzdGVtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    link: "",
   },
   {
     id: 5,
-    img: "",
-    link: "http://lama.dev",
+    img: "https://media.istockphoto.com/photos/standard-quality-control-certification-assurance-concept-picture-id1282633951?b=1&k=20&m=1282633951&s=170667a&w=0&h=k3P0GU07EG4z8gFk6zqaR_cqv5C1wGf_40g4nleXwdE=",
+    link: "",
   },
   {
     id: 6,
-    img: "",
-    link: "http://lama.dev",
+    img: "https://media.istockphoto.com/photos/document-management-concept-picture-id1280171152?b=1&k=20&m=1280171152&s=170667a&w=0&h=imyGZtVqMhXZ8-urg0i06XwIJPRhIAV_IdzarVcyGDI=",
+    link: "",
   },
 ];
